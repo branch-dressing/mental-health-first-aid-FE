7fe1301965ce2a61c167f9ddb726dc948a384229
joelpdurham <joeldur@gmail.com>
2020-04-06 17:05:45 -0700

    successfully able to log out. clears cookies and resets state!

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useLogout } from '../hooks/useLogout';
+import { toGetAuth } from '../selectors/useSelectors';
+import { useSelector } from 'react-redux';
 
 export const Header = () => {
+  const { user } = useSelector(toGetAuth);
+  const { handleLogout } = useLogout();
+
+  const logoutButton = user ? (
+    <button onClick={handleLogout}>Logout</button>
+  ) : (<></>);
+
   return (
     <section>
       <h1>Mental Health First Aid Kit</h1>
       <NavLink to='/profile'>Profile</NavLink>
       <br/>
       <NavLink to='/positive'>Send Positive</NavLink>
+      {logoutButton}
     </section>
   );
 };
